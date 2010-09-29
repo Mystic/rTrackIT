ae3f5a7dc9dfa38e0747164bd6d4fad8847209c1
Mystic <mystic.rick@gmail.com>
2010-09-29 09:55:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RTrackIT::Application.config.secret_token = 'd5afe6839e7a228412dee2a29bd154b22c6cc9f4b15d824f0f0511d501ca20b63e9818e7fd15b2f71bd5abbb1a98e27ae09c6ee57cff214a753a8a7dc8d5b182'
